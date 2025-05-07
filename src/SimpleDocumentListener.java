1fb8b1b021afc3cbae2e2370477b1870db12c040
pinakagwapohaha <145889306+pinakagwapohaha@users.noreply.github.com>
2025-05-07 18:17:05 +0800

    Update SimpleDocumentListener.java

--- src/SimpleDocumentListener.java
+++ src/SimpleDocumentListener.java
@@ -1,1 +1,8 @@
-
+// Helper interface for DocumentListener
+@FunctionalInterface
+interface SimpleDocumentListener extends javax.swing.event.DocumentListener {
+    void update();
+    default void insertUpdate(javax.swing.event.DocumentEvent e) { update(); }
+    default void removeUpdate(javax.swing.event.DocumentEvent e) { update(); }
+    default void changedUpdate(javax.swing.event.DocumentEvent e) { update(); }
+}
